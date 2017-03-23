941d4626837645fc071ab944af91e4f8b0ae6fbc
Anthony Courchesne <anthony.courchesne@mail.mcgill.ca>
2017-03-23 18:50:48 -0400

    BellmanFord done

--- src/BellmanFord.java
+++ src/BellmanFord.java
@@ -20,8 +20,41 @@ public class BellmanFord{
          */
         
         /* YOUR CODE GOES HERE */
-       
+    	source = g.getSource();
+    	distances = new int[g.getNbNodes()];
+    	java.util.Arrays.fill(distances, Integer.MAX_VALUE);
+    	distances[0] = 0;
+    	
+    	predecessors = new int[g.getNbNodes()];
+    	java.util.Arrays.fill(predecessors, -1);
+    	
+    	
+    	for(int i=0; i< g.getNbNodes(); i++){
+    		for(Edge e:g.getEdges()){
+    			if(distances[e.nodes[0]] == Integer.MAX_VALUE){
+    				continue; //We are not going to relax anything if we cant reach first node
+    			}
+    			int newDistance = distances[e.nodes[0]]+ e.weight;
+    	    	if(distances[e.nodes[1]] > newDistance){
+    	    		distances[e.nodes[1]] = newDistance;
+    	    		predecessors[e.nodes[1]] = e.nodes[0];
+    	    	}
+
+    		}
+    	}
+    	//After |V|-1 iteration, if not done then there is a negative cycle
+//    	for(Edge e:g.getEdges()){
+//    		if(distances[e.nodes[1]] > distances[e.nodes[0]] + g.getEdge(e.nodes[0],e.nodes[1]).weight){
+//    			//return false
+//    		}
+//    	}
+    	//return true
+    	
+    	
+    	
     }
+    
+ 
 
     public int[] shortestPath(int destination) throws Exception{
         /*Returns the list of nodes along the shortest path from 
@@ -30,8 +63,27 @@ public class BellmanFord{
          */
 
         /* YOUR CODE GOES HERE (update the return statement as well!) */
+    	
+    	//check if there is a path
+    	if(distances[destination] == Integer.MAX_VALUE){
+    		throw new Exception("No path to the destination exist");
+    	}
+    	
+    	//There is a path, return it
+    	java.util.ArrayList<Integer> pathAL = new ArrayList<Integer>();
+    	int currentNode = destination;
+    	while (currentNode != source){
+    		pathAL.add(0, currentNode);
+    		currentNode = predecessors[currentNode];
+    	}
+    	pathAL.add(0,source);
+    	
+    	int[] path = new int[pathAL.size()];
+    	for(int i=0; i<pathAL.size(); i++){
+    		path[i] = pathAL.get(i);
+    	} 
         
-        return null;
+        return path;
     }
 
     public void printPath(int destination){
